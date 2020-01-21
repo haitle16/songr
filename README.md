bfe497c52a21d582ab3b61c1795df5b907a96ac1
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2020-01-21 12:37:12 -0800

    added readme, tests and albums class

---
+++ README.md
@@ -0,0 +1,19 @@
+## Building Full-Stack Web Apps
+
+### Author: Hai Le
+
+#### Running the App
+
+* In command line run `./gradlew bootRun`
+
+* Open your web browser and go to localhost:8080 to get to the home page.
+
+* localhost:8080/hello/{params} for the hello page, you can enter the params to get back a custom hello page to your name.
+
+* localhost:8080/capitalize/{params} for the capitalize page.
+
+* localhost:8080/albums for the albums page, which renders an array of albums to the page using thymeleaf
+
+#### Test
+* Tested the constructor, getter and setter methods for happy paths.
+
--- src/main/java/com/haile/songr/SongrController.java
+++ src/main/java/com/haile/songr/SongrController.java
@@ -27,5 +27,17 @@ public class SongrController {
         return sentence.toUpperCase();
     }
 
+    @GetMapping("/albums")
+    public String albums(Model m) {
+        Album[] firstAlbum = new Album[] {
+            new Album("Birds In The Trap Sing McKnight", "Travis Scott", 14, 2520,"https://i.pinimg.com/474x/77/fa/27/77fa27724cafb2018ab21f5bee31da8b.jpg"),
+                new Album("You're The One", "Paul Simon", 11, 1980, "https://images-na.ssl-images-amazon.com/images/I/51COc%2BF78IL.jpg"),
+                new Album("Frozen 2", "Idina Menzel, Kristen Bell, Josh Gad",11, 2058, "https://images-na.ssl-images-amazon.com/images/I/81yP4%2BK5g0L._SX522_.jpg")
+        };
+        m.addAttribute("firstAlbum", firstAlbum);
+        return "albums";
+    }
+
+
 
 }
